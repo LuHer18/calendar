70bc42a4efa6e24a9f0cb81d24ef808d4fe9e26a
Luis Herrera <luis1818.lh@gmail.com>
2024-05-13 17:16:06 -0500

    persitencia del state con renovación de token

--- src/api/calendarAPi.js
+++ src/api/calendarAPi.js
@@ -10,6 +10,14 @@ export const calendarAPi = axios.create({
 }) 
 
 //Todo: configurar interceptores
+calendarAPi.interceptors.request.use(config => {
 
+    config.headers= {
+        ...config.headers,
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config
+})
 
 export default calendarAPi;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,38 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { CalendarPage } from "../calendar"
 import { LoginPage } from "../auth"
+import { useAuthStore } from "../hooks"
+import { useEffect } from "react"
 
 
 
 export const AppRouter = () => {
 
-    const authStatus = 'not-authenticated'
+  const {status, checkAuthToken } = useAuthStore()
+
+  useEffect(() => {
+    checkAuthToken();
+  }, [])
+  
+
+  if (status === 'checking') {
+    return(
+      <h3>Cargando...</h3>
+    )
+  }
+
+  
 
   return (
     <Routes>
-        {
-            (authStatus === 'not-authenticated')
-                ?<Route path="/auth/*" element={<LoginPage/>}/>
-                :<Route path="/*" element={<CalendarPage/>}/>
-        }
-        
-        
-        <Route path="/*" element={<Navigate to='/auth/login'/>}/>
+      {
+        (status === 'not-authenticated')
+          ? <Route path="/auth/*" element={<LoginPage />} />
+          : <Route path="/*" element={<CalendarPage />} />
+      }
+
+
+      <Route path="/*" element={<Navigate to='/auth/login' />} />
     </Routes>
   )
 }
