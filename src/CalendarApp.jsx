d2faa6ea574ef3c023d84cda73540733e1dd616a
Luis Herrera <luis1818.lh@gmail.com>
2024-05-14 17:16:28 -0500

    change type of router

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 import { Provider } from "react-redux"
 import { store } from "./store"
@@ -7,9 +7,11 @@ import { store } from "./store"
 export const CalendarApp = () => {
     return (
         <Provider store= {store}>
-            <BrowserRouter>
+            {/* <BrowserRouter> */}
+            <HashRouter>
                 <AppRouter />
-            </BrowserRouter>
+            </HashRouter>
+            {/* </BrowserRouter> */}
         </Provider>
 
     )
