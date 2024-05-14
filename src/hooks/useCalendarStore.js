5b78be6cefb635a9db98aa7ad5181b5e547636ac
Luis Herrera <luis1818.lh@gmail.com>
2024-05-14 11:20:27 -0500

    Save a new event in DB

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,20 +1,24 @@
 import { useDispatch, useSelector } from "react-redux"
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import calendarAPi from "../api/calendarAPi";
 
 
 export const useCalendarStore = () => {
     const {events, activeEvent} = useSelector(state => state.calendar)
+    const {user} = useSelector(state => state.auth)
     const dispatch = useDispatch();
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent))
     }
 
     const starSavigEvent = async(calendarEvent)=> {
-
+        //todo: update event
         if(calendarEvent._id){
             dispatch(onUpdateEvent({...calendarEvent}))
         }else {
-            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime() }))
+            const {data} = await calendarAPi.post('/events', calendarEvent)
+            console.log(data)
+            dispatch(onAddNewEvent({...calendarEvent, id: data.evento.id, user }))
         }
     }
     const starDeletingEvent = () => {
